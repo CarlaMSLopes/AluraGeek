9e34d1d090f50480d990dfdd629691b73c39af09
CarlaMSLopes <carlalopes613@outlook.com>
2024-07-06 18:03:42 -0300

    :heavy_plus_sign: build: cards js e style: cards

---
+++ cards.js
@@ -0,0 +1,66 @@
+
+export const listaDeCards = [
+  {
+    id: 1,
+    imagem: "/assets/luffy.png",
+    tituloDoproduto: "Action Figure Luffy",
+    precoDoProduto: "120",
+    descricaoAcess: "Action Figure Luffy",
+  },
+  {
+    id: 2,
+    imagem: "/assets/sanji.png",
+    tituloDoproduto: "Action Figure Sanji",
+    precoDoProduto: "120",
+    descricaoAcess: "Action Figure Sanji",
+  },
+  {
+    id: 3,
+    imagem: "/assets/zoro.png",
+    tituloDoproduto: "Action Figure Zoro",
+    precoDoProduto: "120",
+    descricaoAcess: "Action Figure Zoro",
+  },
+  {
+    id: 4,
+    imagem: "/assets/gameboy.png",
+    tituloDoproduto: "Gameboy Clássico - 89",
+    precoDoProduto: "450",
+    descricaoAcess: "Gameboy Clássico - 89",
+  },
+  {
+    id:5,
+    imagem: "/assets/ps5.png",
+    tituloDoproduto: "Playstation 5",
+    precoDoProduto: "3.690",
+    descricaoAcess: "Playstation 5",
+  },
+  {
+    id: 6,
+    imagem: "/assets/phone.png",
+    tituloDoproduto: "Fone de ouvido sem fio",
+    precoDoProduto: "245",
+    descricaoAcess: "Fone de ouvido sem fio",
+  },
+  {
+    id: 7,
+    imagem: "/assets/mandalorian.png",
+    tituloDoproduto: "Action Figure Mandalorian",
+    precoDoProduto: "130",
+    descricaoAcess: "Action Figure Mandalorian",
+  },
+  {
+    id: 8,
+    imagem: "/assets/deadpool.png",
+    tituloDoproduto: "Action Figure Deadpool",
+    precoDoProduto: "130",
+    descricaoAcess: "Action Figure Deadpool",
+  },
+  {
+    id: 9,
+    imagem: "/assets/spiderman.png",
+    tituloDoproduto: "Action Figure Spider-man",
+    precoDoProduto: "130",
+    descricaoAcess: "Action Figure spiderman",
+  },
+];
---
+++ main.js
@@ -0,0 +1,30 @@
+import { listaDeCards } from "./cards.js";
+
+const painelDeCards = document.querySelector(".cards_produtos");
+
+listaDeCards.forEach((card) => {
+  painelDeCards.innerHTML += `<div class="card">
+            <img src="${card.imagem}" alt="${card.descricaoAcess}" />
+            <h2 class="${card.id}">${card.tituloDoproduto}</h2>
+            <div class="preco_card">
+              <h3 class="${card.id}">R$ ${card.precoDoProduto}</h3>
+              <button class="botao__apagar">
+                <img src="/assets/delete.svg" alt="Apagar" />
+              </button>
+            </div>
+          </div>`;
+});
+
+// const textosHover = document.querySelectorAll(".titulo__hover");
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach((card) => {
+//   card.addEventListener("mouseenter", () => {
+//     textosHover.forEach((texto) => {
+//       texto.classList.add("cor");
+//     });
+//   });
+//   card.addEventListener("mouseleave", () =>
+//     textosHover.forEach((texto) => texto.classList.remove("cor"))
+//   );
+// });
